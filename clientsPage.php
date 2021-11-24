f94ffc6821ce2d5630d825f6ab8fd455256e09bc
javiermn <44568380+jamani18@users.noreply.github.com>
2021-11-24 13:45:39 +0100

    Create clientsPage.php

---
+++ clientsPage.php
@@ -0,0 +1,8 @@
+<!-- HTML FORM FRAGMENT --> 
+<input id='username' type='text'>
+<input id='password' type='text'>
+
+<?php //Usually event will set on js. ?>
+<button onclick='sendForm'>Send data</button>
+
+<script src="js/clientsPage.js?v=<?=VersionJS?>"></script> 
